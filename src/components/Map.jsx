32f9f9026b5a4220ff31ec5d9d03c5aa3a756739
Rosanno Macabuhay <rosannomacabuhay@Rosanno-Macabuhays-MacBook-Air.local>
2025-12-06 23:05:52 +0800

    Add Footer and Map components; update dependencies for React Icons and React Leaflet

---
+++ src/components/Map.jsx
@@ -0,0 +1,28 @@
+import { Marker, Popup } from "react-leaflet";
+import { MapContainer } from "react-leaflet/MapContainer";
+import { TileLayer } from "react-leaflet/TileLayer";
+
+import "leaflet/dist/leaflet.css";
+
+const Map = () => {
+  return (
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      scrollWheelZoom={false}
+      className="h-[500px] w-full"
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default Map;
